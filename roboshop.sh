9493ff435118ed7f2f0d4fc10c382e204cb05b26
Trinath9395 <trinathdevops0606@gmail.com>
2025-10-18 10:07:04 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -11,25 +11,24 @@ for instance in ${INSTANCES[@]}
 #for instance in $@
 do
 INSTANCE_ID=$(aws ec2 run-instances 
---image-id $AMI_ID 
+--image-id "$AMI_ID" 
 --instance-type t2.micro 
---security-group-ids $SG_ID 
---subnet-id $SUBNET_ID 
---tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" 
+--security-group-ids "$SG_ID" 
+--subnet-id "$SUBNET_ID" 
+--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" 
 --query "Instances[0].InstanceId" 
 --output text)
 
 ```
-if [ $instance != "frontend" ]
-then
+if [ "$instance" != "frontend" ]; then
     IP=$(aws ec2 describe-instances \
-        --instance-ids $INSTANCE_ID \
+        --instance-ids "$INSTANCE_ID" \
         --query "Reservations[0].Instances[0].PrivateIpAddress" \
         --output text)
     RECORD_NAME="$instance.$DOMAIN_NAME"
 else
     IP=$(aws ec2 describe-instances \
-        --instance-ids $INSTANCE_ID \
+        --instance-ids "$INSTANCE_ID" \
         --query "Reservations[0].Instances[0].PublicIpAddress" \
         --output text)
     RECORD_NAME="$DOMAIN_NAME"
@@ -38,22 +37,19 @@ fi
 echo "$instance IP address: $IP"
 
 aws route53 change-resource-record-sets \
---hosted-zone-id $ZONE_ID \
---change-batch '
-{
-    "Comment": "Creating or Updating a record set for cognito endpoint",
-    "Changes": [{
-        "Action": "UPSERT",
-        "ResourceRecordSet": {
-            "Name": "'$RECORD_NAME'",
-            "Type": "A",
-            "TTL": 1,
-            "ResourceRecords": [{
-                "Value": "'$IP'"
-            }]
-        }
-    }]
-}'
+    --hosted-zone-id "$ZONE_ID" \
+    --change-batch "{
+        \"Comment\": \"Creating or Updating a record set for cognito endpoint\",
+        \"Changes\": [{
+            \"Action\": \"UPSERT\",
+            \"ResourceRecordSet\": {
+                \"Name\": \"$RECORD_NAME\",
+                \"Type\": \"A\",
+                \"TTL\": 1,
+                \"ResourceRecords\": [{\"Value\": \"$IP\"}]
+            }
+        }]
+    }"
 ```
 
 done
